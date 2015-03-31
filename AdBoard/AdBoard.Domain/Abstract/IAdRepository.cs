959d61e583d1f43ec28e567f4b0812d0783fb9ec
oleguhcok <oleguchok@gmail.com>
2015-04-01 01:06:30 +0300

    Adding abstract repository

---
+++ AdBoard/AdBoard.Domain/Abstract/IAdRepository.cs
@@ -0,0 +1,14 @@
+﻿using AdBoard.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdBoard.Domain.Abstract
+{
+    public interface IAdRepository
+    {
+        IEnumerable<Ad> Ads { get; }
+    }
+}
