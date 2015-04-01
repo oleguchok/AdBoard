fc62e4d59fc90a91df8512cdc4e1e50006d06bcd
oleguhcok <oleguchok@gmail.com>
2015-04-01 20:43:31 +0300

    Adding Ad controller

---
+++ AdBoard/AdBoard.WebUI/Controllers/AdController.cs
@@ -0,0 +1,24 @@
+﻿using AdBoard.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdBoard.WebUI.Controllers
+{
+    public class AdController : Controller
+    {
+        private IAdRepository repository;
+        
+        public AdController(IAdRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Ads);
+        }
+    }
+}
