1ad60291fbbf44436c327d1e703cff209701712e
oleguhcok <oleguchok@gmail.com>
2015-04-02 00:42:22 +0300

    Adding elemetary view for list of ads

--- AdBoard/AdBoard.WebUI/App_Start/RouteConfig.cs
+++ AdBoard/AdBoard.WebUI/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace AdBoard.WebUI
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: null,
+                url: "Page{page}",
+                defaults: new { controller = "Ad", action = "List" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Ad", action = "List", id = UrlParameter.Optional }
