b6e9a9704f5de918d411f39a15edc5077a6f3cf1
oleguhcok <oleguchok@gmail.com>
2015-04-14 23:54:52 +0300

    Adding Image in UserModel

--- AdBoard/AdBoard.WebUI/Models/IdentityModels.cs
+++ AdBoard/AdBoard.WebUI/Models/IdentityModels.cs
@@ -9,6 +9,9 @@ namespace AdBoard.WebUI.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
